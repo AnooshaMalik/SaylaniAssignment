f47402e2c583177194cffa0767cf3cbd691893aa
AnooshaMalik <anooshaduet@gmail.com>
2020-07-23 11:36:21 -0700

    Initial

---
+++ node.js
@@ -0,0 +1,5 @@
+console.log('connect');
+var mainContent = document.getElementById("main-content");
+console.log(mainContent);
+var childrens = document.querySelectorAll('.render')
+console.log(childrens)
